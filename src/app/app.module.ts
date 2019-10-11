b366909ea51e3b3bee2e37c998e1143cf7fd6bc4
fjgallard <fjdgallardo@gmail.com>
2019-10-11 19:00:34 +0800

    env setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { ComponentsModule } from './components/components.module';
 import { UiModule } from './ui/ui.module';
+import { environment } from 'src/environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -17,7 +23,8 @@ import { UiModule } from './ui/ui.module';
     BrowserAnimationsModule,
     MaterialModule,
     ComponentsModule,
-    UiModule
+    UiModule,
+    AngularFireModule.initializeApp(environment.firestore)
   ],
   providers: [],
   bootstrap: [AppComponent]

d01577e0b51d6941de1984c2e8e3a720e9585a5a
fjgallard <fjdgallardo@gmail.com>
2019-10-12 19:09:49 +0800

    added a feature

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { MaterialModule } from '../material.module';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [SidenavComponent, NavbarComponent],
+  declarations: [SidenavComponent, NavbarComponent, FooterComponent],
   imports: [
     CommonModule,
     MaterialModule
   ],
   exports: [
     SidenavComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ]
 })
 export class UiModule { }

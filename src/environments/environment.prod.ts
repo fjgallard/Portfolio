13d6841a927140b81e31ed94b9a14bffe8e15a0b
fjgallard <fjdgallardo@gmail.com>
2019-10-13 14:49:31 +0800

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore: {
+    apiKey: 'AIzaSyDW8vCBzLhqi635XXmI7TAaJ3dYFVneutY',
+    authDomain: 'fir-project-c19b4.firebaseapp.com',
+    databaseURL: 'https://fir-project-c19b4.firebaseio.com',
+    projectId: 'fir-project-c19b4',
+    storageBucket: 'fir-project-c19b4.appspot.com',
+    messagingSenderId: '299741568151',
+    appId: '1:299741568151:web:c2325bc38d68e26f'
+  }
 };

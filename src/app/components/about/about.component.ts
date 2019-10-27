f81c925875f73fb9b23d22fe62bcf00e3042351b
fjgallard <fjdgallardo@gmail.com>
2019-10-27 12:07:44 +0800

    Download Resume button navigates to resume

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  RESUME_URL = 'https://firebasestorage.googleapis.com/v0/b/fir-project-c19b4.appspot.com/' +
+  'o/Francis%20Jomer%20Gallardo%20-%20Resume.pdf?alt=media&token=4692aa0e-6cc6-44e4-9f61-faf4a18d869e';
+
   skills = ['javascript', 'html', 'css', 'angular', 'java',
             'firestore', 'nosql', 'git', 'confluence', 'jira'];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  downloadResume() {
+    window.open(this.RESUME_URL);
   }
 
 }

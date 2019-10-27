6b1e8f40569b8ea3a1555b2ce027bbecffef376c
fjgallard <fjdgallardo@gmail.com>
2019-10-27 13:40:34 +0800

    Added the resume btn on navbar and sidebar

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RESUME_URL } from 'src/app/models/constants';
 
 @Component({
   selector: 'app-about',
@@ -6,9 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  RESUME_URL = 'https://firebasestorage.googleapis.com/v0/b/fir-project-c19b4.appspot.com/' +
-  'o/Francis%20Jomer%20Gallardo%20-%20Resume.pdf?alt=media&token=4692aa0e-6cc6-44e4-9f61-faf4a18d869e';
 
   skills = ['javascript', 'html', 'css', 'angular', 'java',
             'firestore', 'nosql', 'git', 'confluence', 'jira'];
@@ -18,7 +16,7 @@ export class AboutComponent implements OnInit {
   }
 
   downloadResume() {
-    window.open(this.RESUME_URL);
+    window.open(RESUME_URL);
   }
 
 }
---
+++ src/app/models/constants.ts
@@ -0,0 +1,2 @@
+export const RESUME_URL = 'https://firebasestorage.googleapis.com/v0/b/fir-project-c19b4.appspot.com/' +
+'o/Francis%20Jomer%20Gallardo%20-%20Resume.pdf?alt=media&token=4692aa0e-6cc6-44e4-9f61-faf4a18d869e';

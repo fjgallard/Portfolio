8056f9ea096cdf46ca09e5fa22cbb708c3ded65f
fjgallard <fjdgallardo@gmail.com>
2019-10-27 14:13:47 +0800

    Moved active styling to hover styling

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -1,2 +1,6 @@
 export const RESUME_URL = 'https://firebasestorage.googleapis.com/v0/b/fir-project-c19b4.appspot.com/' +
 'o/Francis%20Jomer%20Gallardo%20-%20Resume.pdf?alt=media&token=4692aa0e-6cc6-44e4-9f61-faf4a18d869e';
+
+export const GITHUB_URL = 'https://www.linkedin.com/in/francis-jomer-gallardo-7bb220103';
+
+export const LINKEDIN_URL = 'https://github.com/fjgallard';

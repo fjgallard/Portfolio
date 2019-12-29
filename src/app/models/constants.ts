7000981fb44e613a55d722a749b2d1b16478e912
fjgallard <fjdgallardo@gmail.com>
2019-12-29 14:40:53 +0800

    Changed Resume

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -1,5 +1,5 @@
-export const RESUME_URL = 'https://firebasestorage.googleapis.com/v0/b/fir-project-c19b4.appspot.com/' +
-'o/Francis%20Jomer%20Gallardo%20-%20Resume.pdf?alt=media&token=4692aa0e-6cc6-44e4-9f61-faf4a18d869e';
+export const RESUME_URL = 'https://firebasestorage.googleapis.com/v0/b/fir-project-c19b4.appspot.com' +
+'/o/Francis%20Jomer%20Gallardo%20-%20Resume.pdf?alt=media&token=5035f8c0-38a1-4f27-b8d8-f8619e5be8f5';
 
 export const GITHUB_URL = 'https://www.linkedin.com/in/francis-jomer-gallardo-7bb220103';
 

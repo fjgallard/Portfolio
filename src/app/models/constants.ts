9b62e01c99e7a23fbd07c5537b5d179a40050450
fjgallard <fjdgallardo@gmail.com>
2020-01-03 16:20:12 +0800

    Resume update

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -1,5 +1,5 @@
 export const RESUME_URL = 'https://firebasestorage.googleapis.com/v0/b/fir-project-c19b4.appspot.com' +
-'/o/Francis%20Jomer%20Gallardo%20-%20Resume.pdf?alt=media&token=5035f8c0-38a1-4f27-b8d8-f8619e5be8f5';
+'/o/Francis%20Jomer%20Gallardo%20-%20Resume.pdf?alt=media&token=e274ad0a-212e-4571-8b95-6530502ffd87';
 
 export const GITHUB_URL = 'https://www.linkedin.com/in/francis-jomer-gallardo-7bb220103';
 
